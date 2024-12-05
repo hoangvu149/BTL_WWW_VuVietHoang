d911d42998453e2c7d08799c51e6ecede91fc8f5
HoangVuViet <hoangvuviet14@gmail.com>
2024-12-05 09:24:26 +0700

    Created

---
+++ src/test/java/vn/edu/iuh/fit/Application/BtlWwwVuVietHoangApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlWwwVuVietHoangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
